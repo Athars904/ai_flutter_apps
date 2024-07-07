b2cacf86daee22df4b5ed24493dd32af58142253
athar <athars904@gmail.com>
2024-07-08 00:38:37 +0500

    Completed project

--- cat_dogs_detection/lib/screens/home_page.dart
+++ cat_dogs_detection/lib/screens/home_page.dart
@@ -1,8 +1,71 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _loading = true;
+  late File _image;
+  List _output = [];  // Initialize _output with an empty list
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) async {
+      setState(() {});
+    });
+  }
+
+  void detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.6,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _output = output!;
+      _loading = false;
+    });
+  }
+
+  Future<void> pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+      detectImage(_image);
+    }
+  }
+
+  Future<void> chooseFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+      detectImage(_image);
+    }
+  }
+
+  Future<void> loadModel() async {
+    await Tflite.loadModel(
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/labels.txt',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +78,19 @@ class HomePage extends StatelessWidget {
             mainAxisSize: MainAxisSize.min, // To center the column vertically
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              const Icon(CupertinoIcons.paw,size: 150,),
-              const SizedBox(
-                height: 30,
+              Column(
+                children: [
+                  _loading
+                      ? const Icon(CupertinoIcons.paw, size: 150)
+                      : Image.file(_image, height: 150),
+                  const SizedBox(height: 10),
+                  _output.isNotEmpty
+                      ? Text('${_output[0]['label']}')
+                      : Container(),
+                  const SizedBox(height: 10),
+                ],
               ),
+              const SizedBox(height: 30),
               const Text(
                 'Cats vs Dogs',
                 style: TextStyle(
@@ -27,11 +99,10 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               GestureDetector(
                 onTap: () {
+                  pickImage();
                   // Handle the click picture action
                 },
                 child: Container(
@@ -49,11 +120,10 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               GestureDetector(
                 onTap: () {
+                  chooseFromGallery();
                   // Handle the choose from gallery action
                 },
                 child: Container(
